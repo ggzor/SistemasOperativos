cb914663c94f89e6e38c2a0f27990662716f301d
ggzor <30713864+ggzor@users.noreply.github.com>
2019-08-09 00:36:50 -0500

    Actualizando codigo de PID's

--- Tarea 1/ProcesosPID.c
+++ Tarea 1/ProcesosPID.c
@@ -5,11 +5,11 @@ int main() {
   int pid = fork();
   int ppid = getppid();
 
-  while (1) {
-    if (pid == 0) {
-      printf("Proceso Hijo  - pid = %6d, ppid = %6d\n", getpid(), ppid);
-    } else {
-      printf("Proceso Padre - pid = %6d, ppid = %6d\n", getpid(), ppid);
-    }
+  if (pid == 0) {
+    printf("Proceso Hijo  - pid = %6d, ppid = %6d\n", getpid(), ppid);
+  } else {
+    printf("Proceso Padre - pid = %6d, ppid = %6d\n", getpid(), ppid);
   }
+  
+  while (1);
 }
