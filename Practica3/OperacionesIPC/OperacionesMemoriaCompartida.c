6c366b0dd99414c5b3ab52cda22b9d442a18dab6
ggzor <30713864+ggzor@users.noreply.github.com>
2019-10-13 18:31:18 -0500

    Terminando operaciones de memoria compartida

--- Practica3/OperacionesIPC/OperacionesMemoriaCompartida.c
+++ Practica3/OperacionesIPC/OperacionesMemoriaCompartida.c
@@ -1,30 +1,87 @@
 #include "Operaciones.h"
+#include "ParserOpciones.h"
 #include "Utilerias.h"
 
 #include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/shm.h>
 
 int operacionesMemoriaCompartida(Parser *parser) {
   char *comando;
   char *comandos[] = { "crear", "escribir", "leer", "liberar" };
+  struct shmid_ds info;
+  int tamano, permisos = 0666, shmid, i;
+  char *memoria;
 
   if (siguienteComando(parser, &comando)) {
     switch (indiceDeCadena(comandos, 4, comando))
     {
     case 0:
-      // TODO: Crear
-      break;
+      if (siguienteEntero(parser, &tamano)) {
+        siguienteEntero(parser, &permisos); 
+        
+        shmid = shmget(IPC_PRIVATE, tamano, IPC_CREAT| permisos);
+        
+        if (shmid >= 0) {
+          printf("Se creó exitosamente el bloque de memoria compartida con shmid %d.\n", shmid);
+          return 0;
+        } else {
+          printf("Ocurrió un error al crear el bloque de memoria compartida.\n");
+        } 
+      } else {
+        printf("No se proporcionó el número de bytes.\n");
+      }
+      return -1;
     
     case 1:
-      // TODO: Escribir 
-      break;
+      if (siguienteEntero(parser, &shmid)) {
+        if (shmctl(shmid, IPC_STAT, &info) >= 0) {
+          memoria = shmat(shmid, 0, 0);
+          fgets(memoria, info.shm_segsz, stdin);
+          printf("\nSe escribieron los datos correctamente.\n");          
+          return 0;
+        } else {
+          printf("No existe un bloque de memoria con el shmid especificado.\n");
+        }
+      } else {
+        printf("No se proporcionó el shmid del bloque de memoria.\n");
+      }
+      return -1;
     
     case 2:
-      // TODO: Leer 
-      break;
+      if (siguienteEntero(parser, &shmid)) {
+        if (shmctl(shmid, IPC_STAT, &info) >= 0) {
+          memoria = shmat(shmid, 0, 0);
+          
+          for (i = 0; memoria[i] != '\0' && i < info.shm_segsz; i++)
+            printf("%c", memoria[i]);
+            
+          return 0;
+        } else {
+          printf("No existe un bloque de memoria con el shmid especificado.\n");
+        }
+      } else {
+        printf("No se proporcionó el shmid del bloque de memoria.\n");
+      }
+      return -1;
 
-    case 4:
-      // TODO: Liberar 
-      break;
+    case 3:
+      if(siguienteEntero(parser, &shmid)) {
+        if (shmctl(shmid, IPC_STAT, &info) >= 0) {
+          if(shmctl(shmid, IPC_RMID, 0) >= 0) {
+            printf("Se ha borrado correctamente el bloque de memoria compartida.\n");
+            return 0;
+          } else {
+            printf("No se pudo borrar el bloque de memoria compartida.\n");
+          } 
+        } else {
+          printf("No existe un bloque de memoria con el shmid especificado.\n");
+        }
+      } else {
+        printf("No se proporcionó el shmid del bloque de memoria.\n");
+      }
+      return -1;
     
     default:
       printf("El comando \"%s\" no se ha reconocido.\nSólo puede ser uno de: crear, escribir, leer o liberar.\n", comando);
